dfdf17927d6d095c04882eec52f2d0d63c567a9b
Nafis <ahmednafis74@gmail.com>
2025-01-15 18:53:29 +0600

    added axios secure on client side

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,40 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_SERVER_ADDRESS,
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  axiosSecure.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        console.log("error from axios interceptor", error);
+        navigate("/login");
+        logOut()
+          .then(() => {})
+          .catch((err) => console.log(err));
+      }
+      return Promise.reject(error);
+    }
+  );
   return axiosSecure;
 };
 
